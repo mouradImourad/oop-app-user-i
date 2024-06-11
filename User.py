f6572536fe7ec384233ffa4ec3974f8b68d3ac69
Mourad Mourad <mourad272@icloud.com>
2024-06-10 23:16:50 -0500

    cahnge the User object's attribute email_address to email to match the craiteria of the test

--- User.py
+++ User.py
@@ -3,9 +3,9 @@
 # 3- create three objects and assign them to three variable
 
 class User:
-    def __init__(self, name, email_address, drivers_license=None):
+    def __init__(self, name, email, drivers_license=None):
         self.name = name
-        self.email_address = email_address
+        self.email = email
         self.drivers_license = drivers_license
 
 user1 = User("Alice Smith", "alice@example.com", "B1234567")
